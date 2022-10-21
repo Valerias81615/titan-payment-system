cde679e9e22c12cd46dd9e908657e520209ed45a
pranavupadhyay99 <pranavu29@gmail.com>
2022-10-20 17:56:20 -0700

    created python file for classes

---
+++ purchase.py
@@ -0,0 +1,27 @@
+class Purchase:
+ 
+    
+    purchase_date = ""
+    purchase_card = ""
+    amount_paid = ""
+    card_amoumt = ""
+    conv_fee = ""
+    payment_status = ""
+ 
+ 
+
+p1 = Purchase()
+
+class User_details:
+    name = ""
+    fullname = ""
+    Phone_no = ""
+    Password = ""
+    country = ""
+    addresss = ""
+
+user1 = User_details()
+
+
+class Card_deets:
+    Amex = 
